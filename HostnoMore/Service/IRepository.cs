e74dd401ac720958548583bd1adaad6db9d2c833
RyanCorbin <ryancorbin3@gmail.com>
2019-09-07 20:14:20 -0700

    Add project files.

---
+++ HostnoMore/Service/IRepository.cs
@@ -0,0 +1,23 @@
+﻿//namespace HostnoMore
+//{
+//internal interface IRepository
+//{
+//}
+//}
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HostnoMore.Models;
+
+namespace HostnoMore.Services
+{
+    public interface IRepository
+    {
+        Task<IList<OrderItem>> GetItem();
+
+        Task<IList<OrderItem>> GetItem(int numberOfItems);
+
+        Task AddItem(OrderItem newOrderItem);
+
+        Task RemoveItem(OrderItem removeOrderItem);
+    }
+}
